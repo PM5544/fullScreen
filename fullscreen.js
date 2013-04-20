131f0b5bc4161582ac48ed0e6faa3092e52eb78d
Joël Kuijten <joel@pm5544.eu>
2013-04-20 21:59:26 +0200

    added the DWTFYW license

--- fullscreen.js
+++ fullscreen.js
@@ -1,3 +1,21 @@
+/*
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+Version 2, December 2004
+
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
+// do GitHub a favour and don't hotlink it from http://pm5544.github.io/fullScreen/fullscreen.js
+
 var fullScreen = ( function ( doc, body, undefined ) {
     var prefixes                = [ "ms", "o", "", "webkit", "moz" ],
         nodeAPIs                = [ "request_" ],
